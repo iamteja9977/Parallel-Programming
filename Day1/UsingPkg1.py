c5ed572eb9d6bf012f2f048d80cc2dfba4ff7076
iamteja9977 <thiru21ssc@gmail.com>
2024-05-10 20:02:15 +0530

    now wrote the code outside of the myPkg folder

---
+++ Day1/UsingPkg1.py
@@ -0,0 +1,11 @@
+# Day1/myPkg/UsingPkg1.py
+
+import myPkg.moduleIntro
+# pip3 install myPkg
+# To install a Python package named myPkg using pip for Python 3
+
+myPkg.moduleIntro.funOne()
+myPkg.moduleIntro.funTwo()
+
+print(myPkg.moduleIntro.dataOne)
+print(myPkg.moduleIntro.dataTwo)
